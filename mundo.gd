e1e5b45525f615acf9d36b0d2fbf64482ab11980
Sérgio Ugoline Junior <sergiouine@gmail.com>
2025-08-29 13:28:06 -0300

    Feito Capitulo 9 indo para o 10

--- mundo.gd
+++ mundo.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var exibidor_de_pontos = $Interface/ExibidorDePontos
 @onready var botao_upgrade_clique = $Interface/MenuDeUpgrades/BotaoUpgradeClique
 @onready var botao_comprar_gerador = $Interface/MenuDeUpgrades/BotaoComprarGerador
+@onready var animador_do_gerador = $GeradorPrincipal/AnimationPlayer
 
 # --- Variáveis do Jogo ---
 # Armazena a quantidade total de "formas" que o jogador possui.
@@ -30,13 +31,14 @@ func _ready():
 	atualizar_interface()
 
 func _on_gerador_principal_gui_input(evento):
-	# Primeiro, verificamos se o evento foi de um botão do mouse,
-	# se foi o botão esquerdo, e se ele acabou de ser pressionado.
 	if evento is InputEventMouseButton and evento.button_index == MOUSE_BUTTON_LEFT and evento.is_pressed():
-		# Se todas as condições forem verdadeiras, executamos a lógica do jogo!
+		# Dispara a animação!
+		animador_do_gerador.play("clique")
+		
+		# O resto da lógica continua igual.
 		formas_totais = formas_totais + upgrade_clique.nivel
-		print("Forma gerada! Total agora é:", formas_totais)
-		atualizar_interface() # Adicione esta linha
+		atualizar_interface()
+
 
 # Uma função nossa, criada para manter a interface do usuário atualizada.
 func atualizar_interface():
